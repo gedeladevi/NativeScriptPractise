5e8d2740d29b04311eb4b08b0d0b6eedb3e92729
gedeladevi <gedeladevi@sblsol.com>
2021-05-21 19:55:15 +0530

    cards implimented

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router';
-
+import { registerElement } from '@nativescript/angular';
+import { CardView } from '@nstudio/nativescript-cardview';
+registerElement('CardView', () => CardView);
 @Component({
   selector: 'Home',
   templateUrl: './home.component.html'
